da69fffcce6f39e335e05a93980103e33286ded4
wangfei0928 <87455709+wangfei0928@users.noreply.github.com>
2022-06-12 23:57:53 +0800

    Add files via upload

---
+++ mybaits-plus-spring/src/main/java/com/wf/mp/simple/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.wf.mp.simple.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wf.mp.simple.pojo.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
